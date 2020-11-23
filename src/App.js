86560206c2dad62613d9e8e66342c18eb09afd95
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-23 17:43:14 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.scss';
-import Header from './components/header';
+import Header from './components/header/index.jsx';
 
 function App() {
   return (
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,10 +7,7 @@ const Header = () => {
         <div className='header__row row'>
           <div className='header__logo logo'>
             <img src={logo} alt='logo' />
-            <div className='logo__info'>
-              <h2 className='logo__title'>REACT PIZZA</h2>
-              <p className='logo__description'>Самая вкусная пицца во вселенной</p>
-            </div>
+            
           </div>
         </div>
       </div>
